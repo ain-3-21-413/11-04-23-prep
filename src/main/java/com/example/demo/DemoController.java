7be31c789f16da4e9c5638234f139114aa401ad5
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-10 18:51:44 +0600

    test with logger

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,8 +1,17 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoController.class); 
+
+	@GetMapping("/test")
+	public void test() {
+		LOGGER.info("test() envoked");
+	}	
 }
