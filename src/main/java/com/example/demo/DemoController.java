61efb0e211631e0db56c43ce6533b5fe3bf86e95
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-10 19:04:50 +0600

    request body `/doesFootballerExistImproved`

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -105,6 +106,30 @@ public class DemoController {
 				f.age() == age &&
 				f.club().name().equals(clubName) &&
 				f.club().league().equals(leagueName)
+			) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@GetMapping("/doesFootballerExistImproved")
+	public boolean doesFootballerExistImproved(@RequestBody Footballer footballer) {
+		List<Footballer> footballers = new ArrayList<>();
+		Club psg = new Club("PSG", "League 1");
+		Club alNassr = new Club("Al Nassr", "Pro League");
+		Footballer messi = new Footballer("Messi", 35, psg);
+		Footballer ronaldo = new Footballer("Ronaldo", 38, alNassr);
+		Footballer neymar = new Footballer("Neymar", 31, psg);
+		footballers.add(messi);
+		footballers.add(ronaldo);
+		footballers.add(neymar);
+		for (Footballer f : footballers) {
+			if (
+				footballer.name().equals(f.name()) &&
+				footballer.age() == f.age() &&
+				footballer.club().name().equals(f.club().name()) &&
+				footballer.club().league().equals(f.club().league())
 			) {
 				return true;
 			}
