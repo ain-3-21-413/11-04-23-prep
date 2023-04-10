327352e1bda6d95c40b6e3beda3da5c6e743620a
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-10 18:54:57 +0600

    list and map

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,15 @@ public class DemoController {
 	@GetMapping("/string")
 	public String string() {
 		return "string";
+	}
+
+	@GetMapping("/list")
+	public List<String> list() {
+		return List.of("Messi", "Ronaldo", "Neymar");
+	}
+
+	@GetMapping("/map")
+	public Map<String, Integer> map() {
+		return Map.of("messi", 35, "ronaldo", 38, "neymar", 31);
 	}
 }
